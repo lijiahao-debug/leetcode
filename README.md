36514aa525233da29d1de3ac01efa91b3e90b976
Li Jiahao <lijh.sdu@gmail.com>
2020-05-25 19:31:59 +0900

    feat(array):1313

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Array
 [1431. Kids With the Greatest Number of Candies](array/1431.cpp)
 [1365. How Many Numbers Are Smaller Than the Current Number](array/1365.cpp)
+[1313. Decompress Run-Length Encoded List](array/1313.cpp)
 
 ## String
 [1108. Defanging an IP Address](string/1108.cpp)
---
+++ array/1313.cpp
@@ -0,0 +1,49 @@
+/*
+1313. Decompress Run-Length Encoded List
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [2,4,4,4]
+Explanation: The first pair [1,2] means we have freq = 1 and val = 2 so we generate the array [2].
+The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4].
+At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+Example 2:
+
+Input: nums = [1,1,2,3]
+Output: [1,3,3]
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVector(vector<int>& v);
+
+class Solution{
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i += 2){
+            for(int j = 0; j < nums[i]; j++){
+                result.push_back(nums[i+1]);
+            }
+        }
+        return result;
+    }   
+};
+
+int main(){
+    vector<int> v = {1,2,3,4};
+    printVector(v);
+    Solution s;
+    vector<int> result = s.decompressRLElist(v);
+    printVector(result);
+    return 0;
+}
+
+void printVector(vector<int>& v){
+    for(auto a : v)
+        cout << a << " ";
+    cout << endl;
+}
