a5d6df3b837b8cb05690d35bef899daf569ee566
Li Jiahao <lijh.sdu@gmail.com>
2020-09-14 21:14:25 +0900

    feat(string):1309

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 ## String
 [1108. Defanging an IP Address](string/1108.cpp)
 [1436. Destination City](string/1436.cpp)
+[1309. Decrypt String from Alphabet to Integer Mapping](string/1309.cpp)
 ## Bit Manipulation
 [1342. Number of Steps to Reduce a Number to Zero](bit/1342.cpp)
 ## Hash Table
---
+++ string/1309.cpp
@@ -0,0 +1,95 @@
+/*
+Given a string s formed by digits ('0' - '9') and '#' . We want to map s to English lowercase characters as follows:
+
+Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+Characters ('j' to 'z') are represented by ('10#' to '26#') respectively. 
+Return the string formed after mapping.
+
+It's guaranteed that a unique mapping will always exist.
+
+ 
+
+Example 1:
+
+Input: s = "10#11#12"
+Output: "jkab"
+Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+Example 2:
+
+Input: s = "1326#"
+Output: "acz"
+Example 3:
+
+Input: s = "25#"
+Output: "y"
+Example 4:
+
+Input: s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
+Output: "abcdefghijklmnopqrstuvwxyz"
+
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution{
+public:
+    string freqAlphabets(string s) {
+        string result = "";
+
+        int len = s.size();
+        for(int i = 0; i < len; i++){
+            if(s[i] == '#'){
+                result.pop_back();
+                result.pop_back();
+
+                int temp = stoi(s.substr(i - 2, 2));
+                result.push_back('a' + temp - 1);
+            }else{
+                int temp = s[i]-48;
+                result.push_back('a'+temp-1);
+            }
+        }
+
+        return result;
+    }
+    string freqAlphabets1(string s) {
+        string result = "";
+
+        int len = s.size();
+        for(int i = 0; i < len; i++){
+            if(i+2 < len && s[i+2] == '#'){
+                int temp = stoi(s.substr(i, 2));
+                result += 'a' + temp - 1; 
+                i += 2;
+            }else{
+                result += s[i] - 49 + 'a'; 
+            }
+        }
+
+        return result;
+    }
+};
+
+int main(){
+
+    Solution sol;
+    string s;
+    string result;
+    s = "10#11#12";
+    result  = sol.freqAlphabets1(s);
+    cout << result << endl;
+    s = "1326#";
+    result  = sol.freqAlphabets1(s);
+    cout << result << endl;
+    s = "25#";
+    result = sol.freqAlphabets1(s);
+    cout << result << endl;
+    s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#";
+    result = sol.freqAlphabets1(s);
+    cout << result << endl;
+
+    return 0;
+}
