6c3947beae492f2924524ddedbb35a7bf0de5330
Li Jiahao <lijh.sdu@gmail.com>
2020-09-29 16:22:20 +0900

    feat(string):KMP algorithm

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # leetcode
 
 ## Array
-[1431. Kids With the Greatest Number of Candies](array/1431.cpp)
-[1365. How Many Numbers Are Smaller Than the Current Number](array/1365.cpp)
-[1313. Decompress Run-Length Encoded List](array/1313.cpp)
+[1431. Kids With the Greatest Number of Candies](array/1431.cpp)  
+[1365. How Many Numbers Are Smaller Than the Current Number](array/1365.cpp)  
+[1313. Decompress Run-Length Encoded List](array/1313.cpp)  
 
 ## String
-[1108. Defanging an IP Address](string/1108.cpp)
-[1436. Destination City](string/1436.cpp)
-[1309. Decrypt String from Alphabet to Integer Mapping](string/1309.cpp)
-[824. Goat Latin](string/824.cpp)
-[1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence](string/1455.cpp)
-[1408. String Matching in an Array](string/1408.cpp)
+[1108. Defanging an IP Address](string/1108.cpp)  
+[1436. Destination City](string/1436.cpp)  
+[1309. Decrypt String from Alphabet to Integer Mapping](string/1309.cpp)  
+[824. Goat Latin](string/824.cpp)  
+[1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence](string/1455.cpp)  
+[1408. String Matching in an Array](string/1408.cpp)  
+[KMP](string/kmp.cpp)  
 ## Bit Manipulation
 [1342. Number of Steps to Reduce a Number to Zero](bit/1342.cpp)
 ## Hash Table
---
+++ string/kmp.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class KMP
+{
+public:
+    vector<int> getPrefixTable(string &pattern);
+    int search(string &text, string &pattern);
+};
+
+int main()
+{
+    KMP kmp;
+    string s;
+    // s = "ABABC";
+    s = "AB";
+
+    string text;
+    text = "ABAACABABCAC";
+    int index = kmp.search(text, s);
+    cout << "index = " << index << endl;
+    return 0;
+}
+
+vector<int> KMP::getPrefixTable(string &pattern)
+{
+    int len = pattern.length();
+    vector<int> prefix_table(len);
+    prefix_table[0] = 0;
+    int i = 1;
+    while (i < pattern.length())
+    {
+        if (pattern[i] == pattern[prefix_table[i - 1]])
+            prefix_table[i] = prefix_table[i - 1] + 1;
+        else
+            prefix_table[i] = 0;
+        i++;
+    }
+    i = len - 1;
+    while (i > 0)
+    {
+        prefix_table[i] = prefix_table[i - 1];
+        i--;
+    }
+    prefix_table[0] = -1;
+    return prefix_table;
+}
+
+int KMP::search(string &text, string &pattern)
+{
+    if (text.length() < pattern.length())
+        return -1;
+
+    vector<int> prefix_table = KMP::getPrefixTable(pattern);
+    int i;
+    for (i = 0; i < prefix_table.size(); i++)
+    {
+        cout << prefix_table[i] << " ";
+    }
+    cout << endl;
+
+    i = 0;
+    int j = 0;
+
+    while (i < text.length())
+    {
+        if (text[i] == pattern[j] && j == pattern.length() - 1)
+            return i - j;
+
+        if(text[i] == pattern[j])
+        {
+            i++;
+            j++;
+        }else
+        {
+            j = prefix_table[j];
+            if(j == -1){
+                i++;
+                j++;
+            }
+        }
+    }
+
+    return -1;
+}
